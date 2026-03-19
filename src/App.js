1a942f179c6685e44d054bd980db5cb7e6e8d823
Osama <osamarafi229@gmail.com>
2026-03-19 14:22:17 +0000

    Updating the Header  to Wellcome to our store

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Product from "./components/Product";
 function App() {
   return (
     <div>
-      <h1>Our Store</h1>
+      <h1>Wellcome to Our Store</h1>
       <Product />
       <Product />
     </div>
